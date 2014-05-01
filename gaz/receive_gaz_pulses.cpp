ca2bec8a5427319c0b7c97de1c06ed941775957e
Sven337 <e5d0ceda@opayq.com>
2014-05-01 17:22:40 +0200

    turn gas receive app into a hub able to send commands to sunrise clock

--- gaz/receive_gaz_pulses.cpp
+++ gaz/receive_gaz_pulses.cpp
@@ -24,6 +24,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <unistd.h>
+#include <poll.h>
 #include "RF24.h"
 
 using namespace std;
@@ -32,13 +33,12 @@ static enum { EMONCMS, DOMAH } OUTPUT_MODE;
 
 // Radio pipe addresses for the 2 nodes to communicate.
 // First pipe is for writing, 2nd, 3rd, 4th, 5th & 6th is for reading...
-// Pipe0 in bytes is "serv1" for mirf compatibility
 const uint64_t pipes[6] = { 0xF0F0F0F0F1LL, 0xF0F0F0F0F0LL, 0xF0F0F0F0A1LL, 0xF0F0F0F0A2LL, 0xF0F0F0F0A3, 0xF0F0F0F0A4 };
 
 // CE and CSN pins On header using GPIO numbering (not pin numbers)
 //RF24 radio("/dev/spidev0.0",8000000,18);  // Setup for GPIO 25 CSN
 //RF24 radio("/dev/spidev0.0",8000000,18);  // Setup for GPIO 25 CSN
-RF24 radio(8, 18, BCM2835_SPI_SPEED_8MHZ);
+RF24 radio(8, 18, BCM2835_SPI_SPEED_4MHZ);
 
 void setup(void)
 {
@@ -54,7 +54,7 @@ void setup(void)
 	radio.setChannel(95);
 	radio.setCRCLength(RF24_CRC_16);
 
-//	radio.openWritingPipe(pipes[0]);
+	radio.openWritingPipe(pipes[0]);
 	radio.openReadingPipe(1,pipes[1]);
 
 	//
@@ -68,11 +68,55 @@ void setup(void)
 	usleep(1000);
 }
 
+void send_led_strip(uint8_t param)
+{
+	uint8_t payload[4];
+	payload[0] = param;
+
+	radio.stopListening();
+	radio.powerUp();
+	if (radio.write(&payload[0], 4)) {
+		fprintf(stderr, "Send successful\n");
+	} else {
+		fprintf(stderr, "Could not send to LED strip\n");
+		radio.printDetails();
+		setup();
+		radio.powerDown();
+		usleep(1000);
+		radio.powerUp();
+	}
+	usleep(1000);
+	radio.startListening();
+}
+
+void led_strip_command(char *cmdbuf)
+{
+	char *p = cmdbuf + strlen("LEDSTRIP ");
+	struct { 
+		const char *cmd;
+		int param;
+	} led_strip_commands[] = {
+			{ "strobe", 3 },
+			{ "sunrise", 1 },
+			{ "sunset" , 2 },
+			{ "stop" , 0 },
+	};
+
+	unsigned int i;
+	for (i = 0; i < sizeof(led_strip_commands)/sizeof(led_strip_commands[0]); i++) {
+		if (!strncmp(led_strip_commands[i].cmd, p, strlen(led_strip_commands[i].cmd))) {
+			send_led_strip(led_strip_commands[i].param);
+			return;
+		}
+	}
+}
+
 void loop(void)
 {
 	char receivePayload[33];
 	uint8_t pipe = 1;
-	
+	struct pollfd input = { 0, POLLIN, 0 };
+	char cmdbuf[150];
 
 	 while ( radio.available( &pipe ) ) {
 
@@ -87,10 +131,15 @@ void loop(void)
 				printf("gas/pulse/%d\n",*((uint16_t *)receivePayload)); 
 				break;
 		}
-//		radio.flush_tx();
-//		radio.flush_rx();
+	}
 
+	if (poll(&input, 1, 1)) {
+		// Data to read on stdin
+		fgets(cmdbuf, sizeof(cmdbuf), stdin);
 
+		if (!strncmp(cmdbuf, "LEDSTRIP ", strlen("LEDSTRIP "))) {
+			led_strip_command(cmdbuf);
+		}
 	}
 
 	usleep(1000);
