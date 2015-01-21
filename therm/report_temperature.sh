81926e3c1a0a54dccf2ec62f451419eec528e069
Sven337 <e5d0ceda@opayq.com>
2015-01-21 20:50:00 +0100

    therm: check CRC before reporting temperature!

--- therm/report_temperature.sh
+++ therm/report_temperature.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-
-FILE=/sys/bus/w1/devices/28-00000*/w1_slave
-
-if [ ! -r $FILE ]; then
-	exit 1
-fi
-
-TEMP=$(sed -n 2p $FILE  | cut -f2 -d=)
-if [ $TEMP -gt 0 ]; then
+ 
+file="/sys/bus/w1/devices/28-00000*/w1_slave"
+function find(){
+ [[ "$1" =~ "$2" ]] && true || false
+}
+ 
+#read device
+CRC=false
+while read curline; do
+		#check crc
+		if( find "$curline" "crc" && find "$curline" "YES" ) then
+				CRC=true
+		fi
+		if($CRC && find "$curline" "t=") then
+			TEMP=`echo $curline|cut -d'=' -f 2`
 #Divide by 1000 and report
-	echo temperature/$(echo $TEMP | head -c 2).$(echo $TEMP | tail -c +3) | /root/home-monitoring-client/data/report_to_hm_web.sh >/dev/null
-fi
+			echo temperature/$(echo $TEMP | head -c 2).$(echo $TEMP | tail -c +3) | /root/home-monitoring-client/data/report_to_hm_web.sh >/dev/null
+		fi
+done <$file
